1fb8b57737fca1617ebb364ab88108ac5f4b7e2f
tora1000 <kota19980114@gmail.com>
2020-11-03 16:01:18 +0900

    商品出品機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,23 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category    
+  belongs_to_active_hash :status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping
+  belongs_to_active_hash :shipping_day
+  
+  with_options presence: true do
+    validates :name, :description, :category, :status, :shipping, :prefecture, :shipping_day
+    with_options format: { with: /\A[0-9]+\z/ } do 
+      validates :price
+    end
+  end
+  
+  validates :category_id, :status_id, :shipping_id, :prefecture_id, :shipping_day, numericality: { other_than: 1 }
+
 end
+
+
