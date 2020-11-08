f874ad45e9c21f5d64d88c8757ee83d4462901d9
tora1000 <kota19980114@gmail.com>
2020-11-08 20:19:53 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:edit, :show,:update]
+  before_action :set_item, only: [:edit, :show,:update,:destroy]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -26,6 +26,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
