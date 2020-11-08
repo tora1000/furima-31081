e9cfb518824a642a3127f64943c4ed2c9af15673
tora1000 <kota19980114@gmail.com>
2020-11-08 18:06:27 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:edit, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -18,13 +20,27 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
+  def update
     @item = Item.find(params[:id])
+    if @item.update(item_params)
+       redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
-
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shipping_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
