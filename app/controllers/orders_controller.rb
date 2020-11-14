7ef12783b77fcdf658263d32a5e424d729df04bd
tora1000 <kota19980114@gmail.com>
2020-11-14 21:53:09 +0900

    購入機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]
-  before_action :set_order, only: [:index, :create]
-  before_action :move_to_index, only: [:create]
+  before_action :set_order, only: [:index, :create, :move_to_index]
+  before_action :move_to_index, only: [:index]
 
   def index
     @item_order = ItemOrder.new
@@ -38,7 +38,10 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index if current_user.id == @item.user_id 
-    redirect_to action: :index if Order.exists?(item_id: [@item.id])
+    if current_user.id == @item.user_id 
+      redirect_to root_path
+    elsif Order.exists?(item_id: [@item.id])
+      redirect_to root_path
+    end 
   end
 end
