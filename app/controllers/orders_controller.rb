81af4a30335f1940f9fe189611fec9826b2fc6ed
tora1000 <kota19980114@gmail.com>
2020-11-14 22:18:10 +0900

    購入機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: [:create]
+  before_action :authenticate_user!, only: [:index,:create]
   before_action :set_order, only: [:index, :create, :move_to_index]
   before_action :move_to_index, only: [:index]
 
